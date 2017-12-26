22dbc8ee246457a43a31fc0a4a01218df76a1e52
Stalin <satthuruz@gmail.com>
2017-12-26 00:23:28 -0500

    Actulizacion controlador image

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 'use strict'
 
-
-// Prueba git
-
-
 var express = require('express');
 var bodyParse = require('body-parser');
 
@@ -13,8 +9,10 @@ var app = express();
 var album_routes = require('./routes/album');
 var image_routes = require('./routes/image');
 
-app.use(bodyParse.urlencoded({extended: false}));
-app.use(bodyParse.json());
+//app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParse.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
+app.use(bodyParse.json({limit: "50mb"}));
+//app.use(bodyParse.json());
 
 // Configurar cabeceras
 
